c2eff8993589df0157e308c71f07b58af6f343e5
gimgisu <gisu1102@gmail.com>
2024-11-01 16:26:01 +0900

    Refactor:: draggable_modal_sheet.dart 리스트 동적 추가 가능 하도록 수정. 리스트 클릭시 탭 이동 수정

--- lib/screen/widgets/draggable_modal_sheet.dart
+++ lib/screen/widgets/draggable_modal_sheet.dart
@@ -1,86 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:shared_map_app/screen/mainfunction/list/list_detail_screen.dart';
 
-class DraggableModalSheet extends StatelessWidget {
+class DraggableModalSheet extends StatefulWidget {
   const DraggableModalSheet({Key? key}) : super(key: key);
+
+  @override
+  _DraggableModalSheetState createState() => _DraggableModalSheetState();
+}
+
+class _DraggableModalSheetState extends State<DraggableModalSheet> {
+  List<Map<String, dynamic>> _listItems = [
+    {
+      "title": "경주",
+      "locationCount": 32,
+      "photoCount": 122,
+      "icon": Icons.star,
+      "color": Colors.pink,
+    },
+    {
+      "title": "서울",
+      "locationCount": 32,
+      "photoCount": 61,
+      "icon": Icons.star,
+      "color": Colors.pink,
+    },
+    {
+      "title": "맛방대지도",
+      "locationCount": null,
+      "photoCount": null,
+      "icon": Icons.food_bank,
+      "color": Colors.amber,
+    },
+  ];
+
+  void _addNewList() {
+    setState(() {
+      _listItems.add({
+        "title": "새로운 리스트 ${_listItems.length + 1}",
+        "locationCount": 0,
+        "photoCount": 0,
+        "icon": Icons.list_alt,
+        "color": Colors.blue,
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return DraggableScrollableSheet(
       initialChildSize: 0.1,
       minChildSize: 0.1,
-      maxChildSize: 0.4,
+      maxChildSize: 0.9,
       builder: (BuildContext context, ScrollController scrollController) {
         return Container(
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
           ),
-          child: ListView(
+          child: ListView.builder(
             controller: scrollController,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(
-                  child: Container(
-                    width: 40,
-                    height: 5,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      borderRadius: BorderRadius.circular(10),
+            itemCount: _listItems.length + 2, // +2 for the draggable handle and "새 리스트 만들기" button
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                // 드래그 핸들
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Container(
+                      width: 40,
+                      height: 5,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[300],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.add_circle_outline, color: Colors.blue),
-                title: Text('새 리스트 만들기'),
-                onTap: () {
-                  // 새 리스트 추가 기능 구현
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.star, color: Colors.pink),
-                title: Text('경주'),
-                subtitle: Row(
+                );
+              } else if (index == 1) {
+                // "새 리스트 만들기" 버튼
+                return ListTile(
+                  leading: Icon(Icons.add_circle_outline, color: Colors.blue),
+                  title: Text('새 리스트 만들기'),
+                  onTap: _addNewList, // 새 리스트 추가
+                );
+              }
+
+              // 동적 리스트 항목
+              final item = _listItems[index - 2];
+              return ListTile(
+                leading: Icon(item["icon"], color: item["color"]),
+                title: Text(item["title"]),
+                subtitle: item["locationCount"] != null
+                    ? Row(
                   children: [
                     Icon(Icons.location_on, size: 14, color: Colors.grey),
-                    Text('32'),
+                    Text('${item["locationCount"]}'),
                     SizedBox(width: 10),
                     Icon(Icons.photo_library, size: 14, color: Colors.grey),
-                    Text('122'),
+                    Text('${item["photoCount"]}'),
                   ],
-                ),
-                onTap: () {
-                  // 경주 리스트 선택 시 동작
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.star, color: Colors.pink),
-                title: Text('서울'),
-                subtitle: Row(
-                  children: [
-                    Icon(Icons.location_on, size: 14, color: Colors.grey),
-                    Text('32'),
-                    SizedBox(width: 10),
-                    Icon(Icons.photo_library, size: 14, color: Colors.grey),
-                    Text('61'),
-                  ],
-                ),
-                onTap: () {
-                  // 서울 리스트 선택 시 동작
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.food_bank, color: Colors.amber),
-                title: Text('맛방대지도'),
-                trailing: ElevatedButton(
+                )
+                    : null,
+                trailing: item["title"] == "맛방대지도"
+                    ? ElevatedButton(
                   onPressed: () {
-                    // 리뷰 남기기 기능
+                    // 리뷰 남기기 기능 구현
                   },
                   child: Text('리뷰 남기러 가기'),
-                ),
-              ),
-            ],
+                )
+                    : null,
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ListDetailScreen(),
+                    ),
+                  );
+                },
+              );
+            },
           ),
         );
       },
